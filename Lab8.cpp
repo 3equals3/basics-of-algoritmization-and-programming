8090dd7d6592537963279ac8da5f925ab609d632
3equals3 <105347293+3equals3@users.noreply.github.com>
2023-11-21 13:14:32 +0300

    Update Lab8.cpp

--- Lab8.cpp
+++ Lab8.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 
 #define PROBLEM 2
@@ -18,38 +19,26 @@
 double S(int n, double x) {
     double result = 0;
     double prev_quotient = 1;
+
+    double body = 1;
     for (int k = 1; k <= n; k++) {
-        result += pow(-1, (double)k) * pow(2 * x, 2 * ((double)k)) / (prev_quotient * 2 * k * (2 * k - 1));
-        prev_quotient = (prev_quotient * 2 * k * (2 * k - 1));
+        body = body * 4 * x * x / (2 * k * (2 * k - 1));
+        result += pow(-1, (double) k) * (body);
     }
     return result;
 }
 
-double Y(double x) {
+double Y(int n, double x) { //n - filler
     return 2 * cos(x) * cos(x) - 2;
 }
 
 double abs_YmS(int n, double x) {
-    return std::abs(Y(x) - S(n, x));
+    return std::abs(Y(n, x) - S(n, x));
 }
 
-void Out_Rez(int function, double a, double b, double h, int n) {
-    switch (function) {
-    case 1: 
-        std::cout << "Y(x):\n";
-        break;
-    case 2:
-        std::cout << "S(x):\n";
-        break;
-    case 3:
-        std::cout << "|Y(x) - S(x)|:\n";
-        break;
-    default:
-        std::cout << "Function is not selected.";
-        return;
-    }
+void Out_Rez(double f(int n, double x), double a, double b, double h, int n) {
     for (double x = a; x <= b; x += h) {
-        std::cout << (function == 1 ? Y(x) : (function == 2 ? S(n, x) : abs_YmS(n, x))) << std::endl;
+        std::cout << f(n, x) << std::endl;
     }
 }
 
@@ -61,22 +50,36 @@ int main(void) {
     INPUT_VALUES("Enter a, b, h, n (write each one separated by a space): ", a >> b >> h >> n); // 0.1 1.0 0.1 6
     INPUT_VALUES("Enter function number(1 is Y(x), 2 is S(x), 3 is abs()): ", function);
 
-    Out_Rez(function, a, b, h, n);
+    switch (function) {
+    case 1:
+        std::cout << "Y(x):\n";
+        break;
+    case 2:
+        std::cout << "S(x):\n";
+        break;
+    case 3:
+        std::cout << "|Y(x) - S(x)|:\n";
+        break;
+    default:
+        std::cout << "Function is not selected.";
+        return 1;
+    }
+
+    Out_Rez(function == 1 ? Y : (function == 2 ? S : abs_YmS), a, b, h, n);
+
     return 0;
-} 
+}
 
 #elif PROBLEM == 2
 
 
 double powerRecursive(double x, int n) { // Рекурсивная функция для вычисления y = x^n
-    if (n == 0) {
-        return 1.0;
-    }
-    else if (n % 2 == 0) {
+    if (n == 0) return 1.0;
+    
+    if (n % 2 == 0) {
         double temp = powerRecursive(x, n / 2);
         return temp * temp;
-    }
-    else {
+    } else {
         return x * powerRecursive(x, n - 1);
     }
 }
